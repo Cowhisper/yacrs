161c12ede3fba65560ce224044fc887065170dd5
niu <kurt@niudeMacBook-Pro.local>
2026-06-23 21:01:03 +0800

    [update] code format for Node class

--- yacrs/config.py
+++ yacrs/config.py
@@ -1,25 +1,25 @@
-import sys
 import copy
 import inspect
 import json
-import yaml
-import toml
-from functools import wraps
 from argparse import ArgumentParser
+from functools import wraps
+
+import toml
+import yaml
 
 
 class Node(dict):
     def __init__(self, init_dict=None, freeze=False):
         init_dict = {} if init_dict is None else init_dict
-        super(Node, self).__init__(init_dict)
-        self.__dict__['__immutable__'] = freeze
+        super().__init__(init_dict)
+        self.__dict__["__immutable__"] = freeze
 
     def is_frozen(self):
-        return self.__dict__['__immutable__']
+        return self.__dict__["__immutable__"]
 
     def freeze(self, freeze=True):
-        self.__dict__['__immutable__'] = freeze
-        for v in self.__dict__.values():
+        self.__dict__["__immutable__"] = freeze
+        for v in self.values():
             if isinstance(v, Node):
                 v.freeze(freeze)
 
@@ -30,95 +30,91 @@ class Node(dict):
         self.set(key, value)
 
     def get(self, key):
-        if not self.has(key):
-            raise AttributeError(
-                "No such attribute '{}' in config node".format(key)
-            )
-        ksegments = key.split('.')
-        if len(ksegments) == 1:
-            return self[ksegments[0]]
-        else:
-            k_suffix = '.'.join(ksegments[1:])
-            return self[ksegments[0]].get(k_suffix)
+        node = self
+        segments = key.split(".")
+        for seg in segments[:-1]:
+            if seg not in node or not isinstance(node[seg], Node):
+                raise AttributeError(
+                    "No such attribute '{}' in config node".format(key)
+                )
+            node = node[seg]
+        last = segments[-1]
+        if last not in node:
+            raise AttributeError("No such attribute '{}' in config node".format(key))
+        return node[last]
 
     def set(self, key, value, do_register=True):
         if self.is_frozen():
-            raise AttributeError(
-                "Attempted to modify on a frozen config node"
-            )
-        ksegments = key.split('.')
-        if len(ksegments) == 1:
-            self[ksegments[0]] = value
+            raise AttributeError("Attempted to modify on a frozen config node")
+        segments = key.split(".")
+        if len(segments) == 1:
+            self[segments[0]] = value
             return
-        else:
-            key_prefix = '.'.join(ksegments[:-1])
-            if not self.has(key_prefix) and do_register:
-                self.register(key_prefix)
-            node = self.get(key_prefix)
-            if isinstance(node, Node):
-                node[ksegments[-1]] = value
-            else:
-                raise AttributeError(f"{key_prefix} is not a Node")
-            return
+        node = self
+        for seg in segments[:-1]:
+            if seg not in node:
+                if do_register:
+                    node[seg] = Node()
+                else:
+                    raise AttributeError(f"{seg} does not exist")
+            elif not isinstance(node[seg], Node):
+                raise AttributeError(f"{seg} is not a Node")
+            node = node[seg]
+        node[segments[-1]] = value
 
     def has(self, key):
-        ksegments = key.split('.')
-        if len(ksegments) == 1:
-            return ksegments[0] in self
-        else:
-            if ksegments[0] not in self:
+        node = self
+        segments = key.split(".")
+        for seg in segments[:-1]:
+            if seg not in node or not isinstance(node[seg], Node):
                 return False
-            if isinstance(self[ksegments[0]], Node):
-                k_suffix = '.'.join(ksegments[1:])
-                return self[ksegments[0]].has(k_suffix)
-            else:
-                return False
+            node = node[seg]
+        return segments[-1] in node
 
     def register(self, key):
         if self.is_frozen():
-            raise AttributeError(
-                "Attempted to modify on a frozen config node"
-            )
-        ksegments = key.split('.')
-        if len(ksegments) > 1:
-            if ksegments[0] not in self:
-                self[ksegments[0]] = Node()
-            if isinstance(self[ksegments[0]], Node):
-                self[ksegments[0]].register('.'.join(ksegments[1:]))
-            else:
-                raise AttributeError(f'{ksegments[0]} is not a Node')
-        else:
-            self[key] = Node()
+            raise AttributeError("Attempted to modify on a frozen config node")
+        node = self
+        for seg in key.split("."):
+            if seg not in node:
+                node[seg] = Node()
+            elif not isinstance(node[seg], Node):
+                raise AttributeError(f"{seg} is not a Node")
+            node = node[seg]
 
     def clone(self):
         return copy.deepcopy(self)
 
-    def pprint(self, skip_prefix='_'):
+    def pprint(self, skip_prefix="_"):
         # print in yaml format
+        lines = []
+
         def _recursive(cfg, indent=0):
-            s = ''
             for k, v in cfg.items():
                 if k.startswith(skip_prefix):
                     continue
                 if isinstance(v, Node):
-                    s += ' ' * indent + k + ':\n'
-                    s += _recursive(v, indent + 2)
+                    lines.append(" " * indent + k + ":")
+                    _recursive(v, indent + 2)
                 else:
-                    s += ' ' * indent + k + ': ' + str(v) + '\n'
-            return s
-        return _recursive(self)
+                    lines.append(" " * indent + k + ": " + str(v))
+
+        _recursive(self)
+        return "\n".join(lines) + "\n" if lines else ""
 
     def delete(self, key):
-        ksegments = key.split('.')
-        if ksegments[0] not in self:
-            raise AttributeError(f'{ksegments[0]} does not exist')
-        if len(ksegments) == 1:
-            del self[ksegments[0]]
-        else:
-            if isinstance(self[ksegments[0]], Node):
-                self[ksegments[0]].delete('.'.join(ksegments[1:]))
-            else:
-                raise AttributeError(f'{ksegments[0]} is not a Node')
+        node = self
+        segments = key.split(".")
+        for seg in segments[:-1]:
+            if seg not in node:
+                raise AttributeError(f"{seg} does not exist")
+            if not isinstance(node[seg], Node):
+                raise AttributeError(f"{seg} is not a Node")
+            node = node[seg]
+        last = segments[-1]
+        if last not in node:
+            raise AttributeError(f"{last} does not exist")
+        del node[last]
 
 
 _C = Node()
@@ -127,63 +123,60 @@ _C._registry = Node()
 
 
 class configurable:
-    UNDBIND = '_'
+    UNDBIND = "_"
+
     def __init__(self, scope=None):
         self.scope = scope
-        
+
     def do_register(self, func):
         scope = self.scope
         signature = inspect.signature(func)
-        cls_name = func.__qualname__.split('.')[0]
-        
+        cls_name = func.__qualname__.split(".")[0]
+
         kwargs = {}
         defaultv = {}
         args = []
         for name, param in signature.parameters.items():
             if isinstance(param.annotation, str):
-                if param.annotation.startswith('.'):
+                if param.annotation.startswith("."):
                     annotation = param.annotation[1:]
                 else:
                     annotation = param.annotation
             else:
                 annotation = name
             if scope is not None:
-                annotation = scope + '.' + annotation
+                annotation = scope + "." + annotation
             kwargs[name] = annotation
             defaultv[name] = param.default
             args.append(name)
 
         _C._configurables[cls_name] = Node(
-            {
-                'kwargs': kwargs,
-                'defaultv': defaultv,
-                'args': args
-            }
-            , freeze=True)
+            {"kwargs": kwargs, "defaultv": defaultv, "args": args}, freeze=True
+        )
         _C._registry[cls_name] = func
 
     def register(self, func):
-        cls_name = func.__qualname__.split('.')[0]
+        cls_name = func.__qualname__.split(".")[0]
         if self.scope is None:
             self.scope = cls_name
         self.do_register(func)
-        
+
         @wraps(func)
         def wrapper(*args, **kwargs):
             func_info = _C._configurables[cls_name].clone()
-            default_kwargs = func_info['kwargs']
-            default_values = func_info['defaultv']
-            args_names = func_info['args']
+            default_kwargs = func_info["kwargs"]
+            default_values = func_info["defaultv"]
+            args_names = func_info["args"]
 
             for k, v in zip(args_names, args):
                 kwargs[k] = v
 
             dkwargs = {}
             for k, v in default_kwargs.items():
-                vs = v.split('.')
+                vs = v.split(".")
                 if vs[0] == configurable.UNDBIND:
                     vs[0] = self.scope
-                v = '.'.join(vs)
+                v = ".".join(vs)
                 if _C.has(v):
                     dkwargs[k] = _C.get(v)
                 elif default_values[k] != inspect._empty:
@@ -193,35 +186,36 @@ class configurable:
                     pass
             dkwargs.update(kwargs)
             return func(**dkwargs)
+
         return wrapper
 
     def __call__(self, func):
         if isinstance(func, str):
             if func not in _C._registry:
-                raise KeyError(f'Unregist module name: {func}.')
+                raise KeyError(f"Unregist module name: {func}.")
             func = _C._registry[func]
-        if '__wrapped__' in func.__dict__:
+        if "__wrapped__" in func.__dict__:
             func = func.__wrapped__
-        cls_name = func.__qualname__.split('.')[0]
+        cls_name = func.__qualname__.split(".")[0]
         if self.scope is None:
             self.scope = cls_name
-            
+
         @wraps(func)
         def wrapper(*args, **kwargs):
             func_info = _C._configurables[cls_name].clone()
-            default_kwargs = func_info['kwargs']
-            default_values = func_info['defaultv']
-            args_names = func_info['args']
+            default_kwargs = func_info["kwargs"]
+            default_values = func_info["defaultv"]
+            args_names = func_info["args"]
 
             for k, v in zip(args_names, args):
                 kwargs[k] = v
 
             dkwargs = {}
             for k, v in default_kwargs.items():
-                vs = v.split('.')
+                vs = v.split(".")
                 if vs[0] == configurable.UNDBIND:
                     vs[0] = self.scope
-                v = '.'.join(vs)
+                v = ".".join(vs)
                 if _C.has(v):
                     dkwargs[k] = _C.get(v)
                 elif default_values[k] != inspect._empty:
@@ -231,34 +225,35 @@ class configurable:
                     pass
             dkwargs.update(kwargs)
             return func(**dkwargs)
+
         return wrapper
-    
+
     def cli(self, func):
         parser = ArgumentParser()
-        parser.add_argument('--config', '-c', type=str, default=None)
-        parser.add_argument('--verbose', '-v', action='store_true')
-        parser.add_argument('options', nargs="*")
+        parser.add_argument("--config", "-c", type=str, default=None)
+        parser.add_argument("--verbose", "-v", action="store_true")
+        parser.add_argument("options", nargs="*")
         args = parser.parse_args()
         if args.config:
-            if args.config.endswith('.json'):
+            if args.config.endswith(".json"):
                 d = json.load(open(args.config))
-            elif args.config.endswith('.yaml') or args.config.endswith('.yml'):
+            elif args.config.endswith(".yaml") or args.config.endswith(".yml"):
                 d = yaml.load(open(args.config), Loader=yaml.FullLoader)
-            elif args.config.endswith('.toml'):
+            elif args.config.endswith(".toml"):
                 d = toml.load(open(args.config))
             else:
-                raise ValueError(f'Unsupported config file type: {args.config}')
+                raise ValueError(f"Unsupported config file type: {args.config}")
             _C.update(d)
 
         for opt in args.options:
-            if '=' not in opt:
+            if "=" not in opt:
                 continue
-            k, v = opt.split('=')
+            k, v = opt.split("=")
             if ":" in k:
                 k, dtype = k.split(":", 1)
                 try:
                     v = eval(dtype)(v)
-                except:
+                except Exception:
                     raise TypeError(f"Cannot convert {v} to {dtype}")
             _C.set(k, v)
         return self(func)
@@ -267,4 +262,5 @@ class configurable:
 def cregister(scope=None):
     def wrapper(func):
         return configurable(scope=scope).register(func=func)
+
     return wrapper
